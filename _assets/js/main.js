94616401044cfce9f71e1316b9feb5b4a8d6d8be
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-01-05 09:22:37 -0200

    E-book

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,10 +1,26 @@
 //= require jquery
 
+/**
+ * Call to action home page
+ */
 $('.call-action').on('click', function() {
     $('html, body').animate({
         scrollTop: $('.email-action').offset().top
     }, 500, function() {
         $('.email-action input').focus();
+    });
+
+    event.preventDefault();
+});
+
+/**
+ * Call to action ebook
+ */
+$('.quote a').on('click', function() {
+    $('html, body').animate({
+        scrollTop: $('.main-left').offset().top
+    }, 500, function() {
+        $('.main-left input').eq(0).focus();
     });
 
     event.preventDefault();
